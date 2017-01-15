f34abcaf4deb189c05e4e37e633a993200c8e0ce
Seongsoon Park <sp3005@nyu.edu>
2017-01-15 00:01:49 -0500

    attempt to make about page work

--- styles/index.js
+++ styles/index.js
@@ -1,13 +1,15 @@
 var hide_lists = function(cb) {
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
+    $('#abouts').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled')
+    $('#projects-btn').removeClass('disabled');
+    $('#about-btn').removeClass('disabled')
 };
 var show_projects = function() {
     $('#posts-btn').removeClass('disabled');
     $('#about-btn').removeClass('disabled');
-    $('#posts', '#about').fadeOut(300, function() {
+    $('#posts', '#abouts').fadeOut(300, function() {
         $('#projects').fadeIn(300)
     });
     $('#projects-btn').addClass('disabled')
@@ -15,7 +17,7 @@ var show_projects = function() {
 var show_posts = function() {
     $('#projects-btn').removeClass('disabled');
     $('#about-btn').removeClass('disabled');
-    $('#projects', '#about').fadeOut(function() {
+    $('#projects', '#abouts').fadeOut(function() {
         $('#posts').fadeIn(300)
     });
     $('#posts-btn').addClass('disabled')
@@ -24,7 +26,7 @@ var show_about = function() {
     $('#projects-btn').removeClass('disabled');
     $('#posts-btn').removeClass('disabled');
     $('#projects', '#posts').fadeOut(function() {
-        $('#about').fadeIn(300)
+        $('#abouts').fadeIn(300)
     });
     $('#about-btn').addClass('disabled')
 };
